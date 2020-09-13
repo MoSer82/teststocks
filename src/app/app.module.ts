83cc9c24f3e9bf6f72606dba6a1ce362b95a834d
mors_02@mail.ru <mors_02@mail.ru>
2020-09-14 00:53:44 +0500

    мелкие правки

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptor } from './token.interceptor';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {
+        path: 'info',
+        pathMatch: 'full',
+        loadChildren: () => import('./stocks-info/stocks-info.module').then(m => m.StocksInfoModule)
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'info'
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'info'
+  }
+]
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +34,20 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
-  providers: [],
+  exports: [
+    RouterModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
